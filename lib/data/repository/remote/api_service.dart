8c7f0d24d109732cecac148f37d3049be0bdf059
dohoan259 <dohoan259@gmail.com>
2021-04-04 23:11:34 +0700

    resolve error api service

--- lib/data/repository/remote/api_service.dart
+++ lib/data/repository/remote/api_service.dart
@@ -3,8 +3,9 @@
 // ignore: import_of_legacy_library_into_null_safe
 import 'package:dio/dio.dart';
 import 'package:flutter_movie_db/data/model/item_model.dart';
+// ignore: import_of_legacy_library_into_null_safe
 import 'package:retrofit/http.dart';
-import 'api_service.g.dart';
+part 'api_service.g.dart';
 
 @RestApi(baseUrl: "http://api.themoviedb.org/3/movie")
 abstract class ApiService {
